0939ab09422ae1eb6b3da8bd66744a525b681fba
Adrian Carpenter <55795671+fizzyade@users.noreply.github.com>
2021-06-22 19:21:38 +0100

    feat: host masking (#88)
    - host masking can be toggled via the ribbon bar.
    - the regular expression host masker uses the standard $number ${name} substitution syntax.
    - fixed the regular expression host masker which had a number of issues that prevented it from working 100% correctly.

--- src/components/RegExHostMasker/RegExHostMasker.cpp
+++ src/components/RegExHostMasker/RegExHostMasker.cpp
@@ -110,14 +110,16 @@ auto Nedrysoft::RegExHostMasker::RegExHostMasker::applyMask(
     Q_UNUSED(hop)
 
     auto expressionMatch = QRegularExpressionMatch();
-    auto tokenExpression = QRegularExpression(R"(\[\w+\])");
-    auto tokenList = QList<QString>();
+    auto tokenExpression = QRegularExpression(R"(\$\{\s*(?<named>.*)\s*\}|(?<dollar>\$\$)|\$(?<number>\d+))");
+    auto tokenTypes = QStringList() << "named" << "dollar" << "number";
     auto searchList = QList<unsigned int>() << static_cast<int>(MatchFlags::MatchHost) << static_cast<int>(MatchFlags::MatchAddress);
     QString *outputString = nullptr;
     bool returnValue = false;
 
     maskedHostName = hostName;
     maskedHostAddress = hostAddress;
+
+    auto maskFlags = QList<MatchFlags>() << MatchFlags::MaskHost << MatchFlags::MaskAddress;
 
     for (auto matchFlag : searchList) {
         for (const auto &maskItem : m_maskList) {
@@ -131,56 +133,88 @@ auto Nedrysoft::RegExHostMasker::RegExHostMasker::applyMask(
 
             auto matchExpression = QRegularExpression(maskItem.m_matchExpression);
 
-            if (static_cast<MatchFlags>(matchFlag) == MatchFlags::MatchHost) {
+            if (!(static_cast<int>(maskItem.m_matchFlags) & static_cast<int>(matchFlag))) {
+                continue;
+            }
+
+            if (matchFlag==static_cast<int>(MatchFlags::MatchHost)) {
                 expressionMatch = matchExpression.match(hostName);
             } else {
-                if (static_cast<MatchFlags>(matchFlag) == MatchFlags::MatchAddress) {
-                    expressionMatch = matchExpression.match(hostAddress);
-                } else {
-                    continue;
-                }
+                expressionMatch = matchExpression.match(hostAddress);
             }
 
-            if (maskItem.m_matchFlags & static_cast<int>(MatchFlags::MaskHost)) {
-                outputString = &maskedHostName;
-            } else {
-                if (maskItem.m_matchFlags & static_cast<int>(MatchFlags::MaskAddress)) {
+            QString replacementString;
+
+            for (auto maskFlag : maskFlags) {
+                if (maskFlag == MatchFlags::MaskHost) {
+                    outputString = &maskedHostName;
+                    replacementString = maskItem.m_hostReplacementString;
+                } else if (maskFlag==MatchFlags::MaskAddress) {
                     outputString = &maskedHostAddress;
+                    replacementString = maskItem.m_addressReplacementString;
                 } else {
                     continue;
                 }
-            }
 
-            if (expressionMatch.hasMatch()) {
-                auto tokenIterator = tokenExpression.globalMatch(maskItem.m_replacementString);
+                if (replacementString.isEmpty()) {
+                    continue;
+                }
 
-                if (!tokenIterator.hasNext()) {
-                    *outputString = maskItem.m_replacementString;
+                if (expressionMatch.hasMatch()) {
+                    auto tokenIterator = tokenExpression.globalMatch(replacementString);
+                    QList<TokenReplacement> tokenReplacementList;
+
+                    if (!tokenIterator.hasNext()) {
+                        *outputString = replacementString;
+
+                        returnValue = true;
+
+                        continue;
+                    }
+
+                    while (tokenIterator.hasNext()) {
+                        auto match = tokenIterator.next();
+
+                        if (match.hasMatch()) {
+                            for (auto tokenType : tokenTypes) {
+                                auto identifier = match.captured(tokenType);
+
+                                if (!identifier.isNull()) {
+                                    TokenReplacement tokenReplacement;
+
+                                    tokenReplacement.type = tokenType;
+                                    tokenReplacement.identifier = identifier;
+
+                                    if (tokenType=="named") {
+                                        tokenReplacement.value = expressionMatch.captured(identifier);
+                                        tokenReplacement.format = "${%1}";
+                                    } else if (tokenType=="number") {
+                                        tokenReplacement.value  = expressionMatch.captured(identifier.toInt());
+                                        tokenReplacement.format = "$%1";
+                                    } else if (tokenType=="dollar") {
+                                        // we catch $$ regardless, so just ignore it here.
+                                        continue;
+                                    } else {
+                                        // ruh roh!
+                                        continue;
+                                    }
+
+                                    tokenReplacementList.append(tokenReplacement);
+                                }
+                            }
+                        }
+                    }
+
+                    *outputString = replacementString;
+
+                    for (const auto &token : tokenReplacementList) {
+                        (*outputString).replace(token.format.arg(token.identifier), token.value);
+                    }
+
+                    (*outputString).replace("$$", "$");
 
                     returnValue = true;
-
-                    continue;
                 }
-
-                tokenList.clear();
-
-                while (tokenIterator.hasNext()) {
-                    auto match = tokenIterator.next();
-
-                    if (match.hasMatch()) {
-                        tokenList.append(match.capturedTexts());
-                    }
-                }
-
-                *outputString = maskItem.m_replacementString;
-
-                for (const auto &token : tokenList) {
-                    auto searchToken = QString(token).replace(QRegularExpression(R"((\[|\]))"), "");
-
-                    ( *outputString ).replace(token, expressionMatch.captured(searchToken));
-                }
-
-                returnValue = true;
             }
         }
     }
@@ -201,7 +235,8 @@ auto Nedrysoft::RegExHostMasker::RegExHostMasker::add(
         unsigned int matchFlags,
         const QString &description,
         const QString &matchExpression,
-        const QString &replacementString,
+        const QString &hostReplacementString,
+        const QString &addressReplacementString,
         const QString &hopString,
         const bool enabled ) -> void {
 
@@ -209,7 +244,8 @@ auto Nedrysoft::RegExHostMasker::RegExHostMasker::add(
 
     item.m_matchFlags = matchFlags;
     item.m_matchExpression = matchExpression;
-    item.m_replacementString = std::move(replacementString);
+    item.m_hostReplacementString = std::move(hostReplacementString);
+    item.m_addressReplacementString = std::move(addressReplacementString);
     item.m_hopString = hopString;
     item.m_description = description;
     item.m_enabled = enabled;
@@ -228,7 +264,8 @@ auto Nedrysoft::RegExHostMasker::RegExHostMasker::saveConfiguration() -> QJsonOb
 
         object.insert("matchFlags", static_cast<int>(item.m_matchFlags));
         object.insert("matchExpression", item.m_matchExpression);
-        object.insert("matchReplacementString", item.m_replacementString);
+        object.insert("hostReplacementString", item.m_hostReplacementString);
+        object.insert("addressReplacementString", item.m_addressReplacementString);
         object.insert("matchHopString", item.m_hopString);
         object.insert("description", item.m_description);
         object.insert("enabled", item.m_enabled);
@@ -248,13 +285,14 @@ auto Nedrysoft::RegExHostMasker::RegExHostMasker::loadConfiguration(QJsonObject 
 
     auto array = configuration["matchItems"].toArray();
 
-    for (auto v : array) {
-        auto item = v.toObject();
+    for (auto variantItem : array) {
+        auto item = variantItem.toObject();
 
         add(item["matchFlags"].toVariant().toUInt(),
             item["description"].toString(),
             item["matchExpression"].toString(),
-            item["matchReplacementString"].toString(),
+            item["hostReplacementString"].toString(),
+            item["addressReplacementString"].toString(),
             item["matchHopString"].toString(),
             item["enabled"].toBool(true) );
     }
--- src/components/RegExHostMasker/RegExHostMasker.h
+++ src/components/RegExHostMasker/RegExHostMasker.h
@@ -42,13 +42,29 @@ namespace Nedrysoft { namespace RegExHostMasker {
             QSet<int> m_hops;
 
             QString m_matchExpression;
-            QString m_replacementString;
+            QString m_hostReplacementString;
             QString m_addressReplacementString;
             QString m_hopString;
             QString m_description;
             bool m_enabled;
 
             unsigned int m_matchFlags;
+
+            //! @endcond
+    };
+
+    /**
+     * @brief       The TokenReplacement class is used internally by the masker to provide substitution of
+     *              capture groups.
+     */
+    class TokenReplacement {
+        public:
+            //! @cond
+
+            QString type;
+            QString identifier;
+            QString value;
+            QString format;
 
             //! @endcond
     };
@@ -112,7 +128,8 @@ namespace Nedrysoft { namespace RegExHostMasker {
              * @param[in]   matchFlags the bit mask of values from MatchFlags to control masking.
              * @param[in]   description the description displayed for this masker.
              * @param[in]   matchExpression the regular expression used for name matching.
-             * @param[in]   replacementString the string to be used for replacement.
+             * @param[in]   hostReplacementString the string to be used for host name replacement.
+             * @param[in]   addressReplacementString the string to be used for host address replacement.
              * @param[in]   hopString hop string listing hops this mask is valid for. (optional)
              * @param[in]   enabled whether this entry is enabled.
              */
@@ -120,7 +137,8 @@ namespace Nedrysoft { namespace RegExHostMasker {
                     unsigned int matchFlags,
                     const QString &description,
                     const QString &matchExpression,
-                    const QString &replacementString,
+                    const QString &hostReplacementString,
+                    const QString &addressReplacementString,
                     const QString &hopString,
                     const bool enabled= true
             ) -> void;
